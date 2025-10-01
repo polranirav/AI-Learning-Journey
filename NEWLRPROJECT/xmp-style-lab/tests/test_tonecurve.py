fd27b87d154d3ff1253915d6a9880c4d013eae02
Nirav Polara <Polranirav@gmail.com>
2025-10-01 17:25:03 -0400

    Delete NEWLRPROJECT directory

--- NEWLRPROJECT/xmp-style-lab/tests/test_tonecurve.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from xmp_style_lab.parsing.tonecurve import expand_curve
-
-
-def test_expand_curve_basic():
-    assert expand_curve("0, 0, 255, 255") == [0.0, 0.0, 255.0, 255.0]
-
-
